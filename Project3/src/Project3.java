06c4ee55ca6190d556091f4105199c7dcfc304a1
Dylan Moon <moontruggy@gmail.com>
2022-11-17 21:23:23 -0800

    new Project 3

--- Project3/src/Project3.java
+++ Project3/src/Project3.java
@@ -1,63 +1,88 @@
 // Dylan Moon
 // Project 3
-// 12/01/2022  //TODO: this needs to be the assignment's due date.
-// Find the number of potential Binary Search Trees in 1 node - N nodes
+// 11/30/2022
 
-import java.io.IOException;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.HashMap;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Project3 {
-    public static void main(String[] args) throws IOException {
-        var input = Integer.parseInt(Files.readAllLines(Paths.get("input.txt")).get(0));
+    //TODO: DO NOT FORGET TO SET INPUT_FILE TO "input.txt" BEFORE SUBMITTING
+    final static String INPUT_FILE = "input.txt";
+
+    public static void main(String[] args) {
+        final var input = GetInput();
         System.out.println(input);
-        var cn = new CatalanNumber();
-        System.out.println(cn.Calculate(input));
+        System.out.println("Target :" + input.target);
+        BinarySearch2d(input.target, input.arr);
+    }
+
+    /**
+     * Reads a file in the same directory as the java file
+     * @implNote filename and path gotten from {@link Project3#INPUT_FILE} : {@value INPUT_FILE}
+     * @return 2D array of {@link Integer}
+     * @author Dylan Moon
+     */
+    private static IDK GetInput() {
+        try (var sc = new Scanner(new File(INPUT_FILE))){
+            var rows = sc.nextInt();
+            var columns = sc.nextInt();
+            var target = sc.nextInt();
+            var result = new IDK(rows, columns, target, new int[rows][columns]);
+            for (int row = 0; row < rows; row++) {
+                for(int column = 0; column < columns; column++){
+                    result.arr[row][column]=sc.nextInt();
+                }
+            }
+            return result;
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            return new IDK(0,0,0,new int[0][0]);
+        }
+    }
+
+    private static ArrayList<Object> BinarySearch2d(final int target, final int[][] arr){
+        return BinarySearch2dRecursive(target, arr, 0, arr.length -1, 0, arr[0].length -1);
+    }
+    private static ArrayList<Object> BinarySearch2dRecursive(final int target, final int[][] arr, final int rlb, final int rub, final int clb, final int cub){
+        if(rub < rlb || cub < clb)return new ArrayList<Object>(){{add(false);}};
+        if(rub == rlb || cub == clb) return new ArrayList<Object>(){{add(false);}};
+        System.out.println("BinSearch | rlb: " + rlb + " rub: " + rub + " clb: " + clb + " cub: " + cub);
+        var row = (rlb + rub) / 2;
+        int column = (clb + cub) / 2;
+        var curr = arr[row][column];
+        System.out.println("Checking position: (" + row + "," + column + ") = " + curr);
+        if(curr == target) return new ArrayList<Object>(){{add(true); add(row); add(column);}};
+        else if (curr < target) {
+            var r1 = BinarySearch2dRecursive(target, arr, rlb, row, column + 1, cub);
+            var r2 = BinarySearch2dRecursive(target, arr, row + 1, rub, clb, column);
+            var r3 = BinarySearch2dRecursive(target, arr, row + 1, rub, column + 1, cub);
+        }
+        else{
+            var r1 = BinarySearch2dRecursive(target, arr, rlb, row - 1, clb, column);
+            var r2 = BinarySearch2dRecursive(target, arr, rlb, row -1, column + 1, cub);
+            var r3 = BinarySearch2dRecursive(target, arr, row, rub, clb, column - 1);
+        }
+        return new ArrayList<>(){{add(false);}};
     }
 }
 
+final class IDK{
+    final int rows;
+    final int columns;
+    final int target;
+    final int[][] arr;
 
-class CatalanNumber {
-
-    private BigInteger BigFactorial(BigInteger n) throws IllegalArgumentException{
-        if( n.signum() < 0) throw new IllegalArgumentException("BigFactorial argument must be positive");
-        return BigFactorialHelper(n, new HashMap<BigInteger, BigInteger>());
-    }
-    private BigInteger BigFactorialHelper(BigInteger n, HashMap<BigInteger,BigInteger> map){
-        if(n.equals(BigInteger.ZERO) || n.equals(BigInteger.ONE)) return BigInteger.ONE;
-        if(map.containsKey(n)){
-            return map.get(n);
-        }
-        else{
-            map.put(n, n.multiply(BigFactorialHelper(n.subtract(BigInteger.ONE), map)));
-            return map.get(n);
-        }
+    public IDK(final int r, final int c, final int t, final int[][] a){
+        rows = r;
+        columns = c;
+        target = t;
+        arr = a;
     }
 
-    public BigInteger Calculate(BigInteger n) throws IllegalArgumentException{
-        return BigFactorial(n.multiply(BigInteger.TWO)).divide(BigFactorial(n).multiply(BigFactorial(n.add(BigInteger.ONE))));
-    }
-
-    public BigInteger Calculate(int n) throws IllegalArgumentException{
-        return Calculate(BigInteger.valueOf(n));
-    }
-
-    public <T> BigInteger Calculate(T input) throws IllegalArgumentException{
-        return Calculate(new BigInteger(input.toString()));
-    }
-
-    public <T> void Test(T input) {
-        try{
-            System.out.print("input: " + input.toString());
-            System.out.print(" --> " + Calculate(input));
-        }
-        catch (Exception e){
-            System.err.print("Error: " + e);
-        }
-        finally {
-            System.out.println();
-        }
+    @Override
+    public String toString(){
+        return Arrays.deepToString(arr);
     }
 }
---
+++ Project4/src/Project4.java
@@ -0,0 +1,63 @@
+// Dylan Moon
+// Project 4
+// 12/01/2022  //TODO: this needs to be the assignment's due date.
+// Find the number of potential Binary Search Trees in 1 node - N nodes
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class Project4 {
+    public static void main(String[] args) throws IOException {
+        var input = Integer.parseInt(Files.readAllLines(Paths.get("input.txt")).get(0));
+        System.out.println(input);
+        var cn = new CatalanNumber();
+        System.out.println(cn.Calculate(input));
+    }
+}
+
+
+class CatalanNumber {
+
+    private BigInteger BigFactorial(BigInteger n) throws IllegalArgumentException{
+        if( n.signum() < 0) throw new IllegalArgumentException("BigFactorial argument must be positive");
+        return BigFactorialHelper(n, new HashMap<BigInteger, BigInteger>());
+    }
+    private BigInteger BigFactorialHelper(BigInteger n, HashMap<BigInteger,BigInteger> map){
+        if(n.equals(BigInteger.ZERO) || n.equals(BigInteger.ONE)) return BigInteger.ONE;
+        if(map.containsKey(n)){
+            return map.get(n);
+        }
+        else{
+            map.put(n, n.multiply(BigFactorialHelper(n.subtract(BigInteger.ONE), map)));
+            return map.get(n);
+        }
+    }
+
+    public BigInteger Calculate(BigInteger n) throws IllegalArgumentException{
+        return BigFactorial(n.multiply(BigInteger.TWO)).divide(BigFactorial(n).multiply(BigFactorial(n.add(BigInteger.ONE))));
+    }
+
+    public BigInteger Calculate(int n) throws IllegalArgumentException{
+        return Calculate(BigInteger.valueOf(n));
+    }
+
+    public <T> BigInteger Calculate(T input) throws IllegalArgumentException{
+        return Calculate(new BigInteger(input.toString()));
+    }
+
+    public <T> void Test(T input) {
+        try{
+            System.out.print("input: " + input.toString());
+            System.out.print(" --> " + Calculate(input));
+        }
+        catch (Exception e){
+            System.err.print("Error: " + e);
+        }
+        finally {
+            System.out.println();
+        }
+    }
+}
