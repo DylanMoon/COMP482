58407497376989e756c5ba72734f85be89bf2ace
Dylan Moon <moontruggy@gmail.com>
2022-11-20 00:40:37 -0800

    updated test

--- Project3/src/Tests.java
+++ Project3/src/Tests.java
@@ -8,18 +8,19 @@ public class Tests {
                 arr[i][j] = count++;
             }
         }
-        for (int[] ints : arr) {
+        /*for (int[] ints : arr) {
             for (int j = 0; j < arr[0].length; j++) {
                 System.out.printf("%3d ", ints[j]);
             }
             System.out.println();
-        }
+        }*/
         var startTime = System.nanoTime();
-        var curr = -1;
-        while(curr <= count){
-            Project3.BinarySearch2d(curr++, arr);
-        }
-//        System.out.println(Project3.BinarySearch2d(200, arr));
+//        var curr = -1;
+//        while(curr <= count){
+//            System.out.println(Project3.BinarySearch2d(curr++, arr));
+//            Project3.BinarySearch2d(curr++, arr);
+//        }
+        System.out.println(Project3.BinarySearch2d(0, arr));
         var stopTime = System.nanoTime();
 
         System.out.println("elapsed: " + (stopTime - startTime) / 1000000 + "ms");
