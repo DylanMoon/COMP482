ed6c58f4227754fa771b3450a635470eb8ef527e
Dylan Moon <moontruggy@gmail.com>
2022-11-20 00:33:30 -0800

    added a test class

--- Project3/src/Project3.java
+++ Project3/src/Project3.java
@@ -10,7 +10,10 @@ public class Project3 {
     //TODO: DO NOT FORGET TO SET INPUT_FILE TO "input.txt" BEFORE SUBMITTING
     final static String INPUT_FILE = "input.txt";
 
+    private static int numCalls;
+
     public static void main(String[] args) {
+
         final var input = GetInput();
 //        System.out.println(input);
 //        System.out.println("Target :" + input.target);
@@ -41,49 +44,47 @@ public class Project3 {
         }
     }
 
-    private static String BinarySearch2d(final int target, final int[][] arr){
+    public static String BinarySearch2d(final int target, final int[][] arr){
+//        numCalls = 0;
         var result = BinarySearch2dRecursive(target, arr, 0, arr.length -1, 0, arr[0].length -1);
-        if(result.Found) return result.Row + " " + result.Column;
-        return "NOT FOUND";
+//        System.out.println("Number of calls: " + numCalls);
+        return result.toString();
     }
     private static Result BinarySearch2dRecursive(final int target, final int[][] arr, final int rlb, final int rub, final int clb, final int cub){
-//        System.out.println("BinarySearch2dRecursive");
-        if(rub < rlb || cub < clb)return Result.False();
+//        numCalls++;
+        if(rub < rlb || cub < clb)return Result.NotFound();
         if(rub == rlb) return RowBinarySearch(target, arr, clb, cub, rub);
         if(cub == clb) return ColumnBinarySearch(target, arr, rlb, rub, cub);
         var row = (rlb + rub) / 2;
         int column = (clb + cub) / 2;
         var curr = arr[row][column];
-        if(curr == target) return Result.True(row, column);
+        if(curr == target) return Result.Found(row, column);
         else if (curr < target) {
             var r1 = BinarySearch2dRecursive(target, arr, rlb, row, column + 1, cub);
-            if(r1.Found)return r1;
+            if(r1 instanceof Found)return r1;
             var r2 = BinarySearch2dRecursive(target, arr, row + 1, rub, clb, column);
-            if(r2.Found)return r2;
-            var r3 = BinarySearch2dRecursive(target, arr, row + 1, rub, column + 1, cub);
-            if(r3.Found)return r3;
+            if(r2 instanceof Found)return r2;
+            return BinarySearch2dRecursive(target, arr, row + 1, rub, column + 1, cub);
         }
         else{
             var r1 = BinarySearch2dRecursive(target, arr, rlb, row - 1, clb, column);
-            if(r1.Found)return r1;
+            if(r1 instanceof Found)return r1;
             var r2 = BinarySearch2dRecursive(target, arr, rlb, row -1, column + 1, cub);
-            if(r2.Found)return r2;
-            var r3 = BinarySearch2dRecursive(target, arr, row, rub, clb, column - 1);
-            if(r3.Found)return r3;
+            if(r2 instanceof Found)return r2;
+            return BinarySearch2dRecursive(target, arr, row, rub, clb, column - 1);
         }
-        return Result.False();
     }
 
     private static Result RowBinarySearch(final int target, final int[][] arr, final int lowerBound, final int upperBound, final int row){
-//        System.out.println("RowBinarySearch");
-        if(lowerBound > upperBound) return Result.False();
+//        numCalls++;
+        if(lowerBound > upperBound) return Result.NotFound();
         if(lowerBound == upperBound) return arr[row][lowerBound] == target ?
-                Result.True(row, lowerBound):
-                Result.False();
+                Result.Found(row, lowerBound):
+                Result.NotFound();
         var index = (lowerBound + upperBound) /2;
         var curr = arr[row][index];
         if(curr == target){
-            return Result.True(row, index);
+            return Result.Found(row, index);
         }
         else if(curr < target){
             return RowBinarySearch(target, arr, index + 1, upperBound, row);
@@ -94,15 +95,15 @@ public class Project3 {
     }
 
     private static Result ColumnBinarySearch(final int target, final int[][] arr, final int lowerBound, final int upperBound, final int column){
-//        System.out.println("ColumnBinarySearch");
-        if(lowerBound > upperBound) return new Result(false);
+//        numCalls++;
+        if(lowerBound > upperBound) return Result.NotFound();
         if(lowerBound == upperBound) return arr[lowerBound][column] == target ?
-                Result.True(lowerBound, column):
-                Result.False();
+                Result.Found(lowerBound, column):
+                Result.NotFound();
         var index = (lowerBound + upperBound) /2;
         var curr = arr[index][column];
         if(curr == target){
-            return Result.True(index, column);
+            return Result.Found(index, column);
         }
         else if(curr < target){
             return ColumnBinarySearch(target, arr, index + 1, upperBound, column);
@@ -112,28 +113,32 @@ public class Project3 {
         }
     }
 }
+abstract class Result{
+    public static Result Found(final int row, final int column){
+        return new Found(row, column);
+    }
+    public static Result NotFound(){
+        return new NotFound();
+    }
+}
 
-final class Result{
-    final boolean Found;
+final class Found extends Result{
     final int Row;
     final int Column;
-    public Result(final boolean found){
-        Found = found;
-        Row=0;
-        Column = 0;
+    Found(final int row, final int column){
+        Row=row + 1;
+        Column = column + 1;
     }
-    public Result(final boolean found, final int row, final int column){
-        Found = found;
-        Row = row;
-        Column = column;
+    @Override
+    public String toString() {
+        return Row + " " + Column;
     }
+}
 
-    public static Result False(){
-        return new Result(false);
-    }
-
-    public static Result True(final int row, final int column){
-        return new Result(true, row + 1, column + 1);
+final class NotFound extends Result{
+    @Override
+    public String toString() {
+        return "NOT FOUND";
     }
 }
 
---
+++ Project3/src/Tests.java
@@ -0,0 +1,28 @@
+public class Tests {
+
+    public static void main(String[] args) {
+        var arr = new int[100][100];
+        var count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = count++;
+            }
+        }
+        for (int[] ints : arr) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.printf("%3d ", ints[j]);
+            }
+            System.out.println();
+        }
+        var startTime = System.nanoTime();
+        var curr = -1;
+        while(curr <= count){
+            Project3.BinarySearch2d(curr++, arr);
+        }
+//        System.out.println(Project3.BinarySearch2d(200, arr));
+        var stopTime = System.nanoTime();
+
+        System.out.println("elapsed: " + (stopTime - startTime) / 1000000 + "ms");
+
+    }
+}
